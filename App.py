42a112a01ea4c73c079c0590780fe9ce0818eb9d
Neeraj Verma <241379+numberformat@users.noreply.github.com>
2024-06-23 23:55:58 -0400

    getting sqlalchemy to create the schema

--- App.py
+++ App.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///employee.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+# start python and type the following to create tables in the database.
+#>>> from App import app, db
+#>>> with app.app_context():
+#...     db.create_all()
+# check the ./instance folder.
 
 @app.route('/')
 def Index():
